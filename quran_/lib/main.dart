b1eaf5ffe9bb211f7f939156b2885c3b8a6acd84
yahyokhan <yyaxyoxon13@gmail.com>
2024-11-25 22:16:59 +0500

    mini ilova quran 100% bitti

--- quran_/lib/main.dart
+++ quran_/lib/main.dart
@@ -301,8 +301,9 @@ class _SahifaState extends State<Sahifa> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) =>
-                            SurahDetail(surahData: data[index]),
+                        builder: (context) => SurahData(
+                          salom: data[index],
+                        ),
                       ),
                     );
                   },
@@ -364,36 +365,61 @@ class _SahifaState extends State<Sahifa> {
   }
 }
 
-class SurahDetail extends StatefulWidget {
-  final Map<String, dynamic> surahData;
+class SurahData extends StatefulWidget {
+  final Map<String, dynamic> salom;
 
-  SurahDetail({required this.surahData});
+  const SurahData({required this.salom});
 
   @override
-  _SurahDetailState createState() => _SurahDetailState();
+  _SurahDataState createState() => _SurahDataState();
 }
 
-class _SurahDetailState extends State<SurahDetail> {
+class _SurahDataState extends State<SurahData> {
+  List<Map<String, dynamic>> ayahs = [];
+  bool isLoading = true;
   AudioPlayer audioPlayer = AudioPlayer();
   Duration duration = Duration.zero;
   Duration position = Duration.zero;
+  String? currentAudioUrl;
+  int currentAyahIndex = 0;
+  bool isPlaying = false;
 
   @override
   void initState() {
     super.initState();
-    // Load the audio for this Surah
-    String? audioUrl = widget.surahData['surah'] as String?;
+    fetchSurahDetails();
+    setupAudioCompletionListener();
+  }
 
-    // Check if audioUrl is not null
-    if (audioUrl != null && audioUrl.isNotEmpty) {
-      setupAudio(audioUrl);
+  Future<void> fetchSurahDetails() async {
+    final int surahNumber = widget.salom['number'];
+    final response = await http.get(
+      Uri.parse("https://api.alquran.cloud/v1/surah/$surahNumber/ar.alafasy"),
+    );
+
+    if (response.statusCode == 200) {
+      final jsondata = jsonDecode(response.body);
+      setState(() {
+        ayahs = (jsondata['data']['ayahs'] as List)
+            .map((item) => Map<String, dynamic>.from(item))
+            .toList();
+        isLoading = false;
+      });
     } else {
-      print('Audio URL is null or empty');
+      throw Exception("Failed to load surah details");
     }
   }
 
-  void setupAudio(String url) async {
-    await audioPlayer.setSourceUrl(url);
+  void setupAudio(String url, int index) async {
+    if (currentAudioUrl != url) {
+      await audioPlayer.setSourceUrl(url);
+      currentAudioUrl = url;
+    }
+
+    setState(() {
+      currentAyahIndex = index;
+      isPlaying = true;
+    });
 
     audioPlayer.onDurationChanged.listen((Duration newDuration) {
       setState(() {
@@ -406,6 +432,29 @@ class _SurahDetailState extends State<SurahDetail> {
         position = newPosition;
       });
     });
+
+    audioPlayer.resume();
+  }
+
+  void setupAudioCompletionListener() {
+    // Listen for audio completion
+    audioPlayer.onPlayerComplete.listen((event) {
+      if (currentAyahIndex < ayahs.length - 1) {
+        setState(() {
+          currentAyahIndex++;
+        });
+        // Play the next Ayah automatically
+        String nextAudioUrl = ayahs[currentAyahIndex]['audio'] ?? '';
+        if (nextAudioUrl.isNotEmpty) {
+          setupAudio(nextAudioUrl, currentAyahIndex);
+        }
+      } else {
+        // Stop playing if it's the last Ayah
+        setState(() {
+          isPlaying = false;
+        });
+      }
+    });
   }
 
   @override
@@ -416,302 +465,76 @@ class _SurahDetailState extends State<SurahDetail> {
 
   @override
   Widget build(BuildContext context) {
-    final ayahs = widget.surahData['ayahs'] as List<dynamic>;
+    if (isLoading) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.salom['englishName'] ?? 'Surah'),
+        ),
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.surahData['englishName'] ?? 'Surah'),
+        title: Text(widget.salom['englishName'] ?? 'Surah'),
       ),
-      body: Column(
-        children: [
-          // Slider for audio control
-          Slider(
-            min: 0,
-            max: duration.inSeconds.toDouble(),
-            value: position.inSeconds
-                .toDouble()
-                .clamp(0.0, duration.inSeconds.toDouble()),
-            onChanged: (value) async {
-              final newPosition = Duration(seconds: value.toInt());
-              await audioPlayer.seek(newPosition);
-              setState(() {
-                position = newPosition;
-              });
-            },
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: ayahs.length,
-              itemBuilder: (context, index) {
-                final ayah = ayahs[index];
-                return Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: ListTile(
-                    title: Text(
-                      "${ayah['text'] ?? ''}",
-                      style: TextStyle(fontSize: 20),
-                    ),
+      body: Padding(
+        padding: EdgeInsets.all(
+          10,
+        ),
+        child: ListView.builder(
+          itemCount: ayahs.length,
+          itemBuilder: (context, index) {
+            final ayah = ayahs[index];
+            bool isCurrentAyah = index == currentAyahIndex;
+
+            return Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Card(
+                color: isCurrentAyah
+                    ? const Color.fromARGB(255, 0, 244, 8)
+                    : Colors.white,
+                child: ListTile(
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${ayah['text'] ?? ''}",
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ],
                   ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (audioPlayer.state == PlayerState.playing) {
-            audioPlayer.pause();
-          } else {
-            audioPlayer.resume();
-          }
-        },
-        child: Icon(
-          audioPlayer.state == PlayerState.playing
-              ? Icons.pause
-              : Icons.play_arrow,
+                  trailing: IconButton(
+                    icon: Icon(
+                      isCurrentAyah && isPlaying
+                          ? Icons.pause
+                          : Icons.play_arrow,
+                    ),
+                    onPressed: () {
+                      if (isCurrentAyah && isPlaying) {
+                        // Pause if currently playing this Ayah
+                        audioPlayer.pause();
+                        setState(() {
+                          isPlaying = false;
+                        });
+                      } else {
+                        // Play the selected Ayah
+                        setupAudio(ayah['audio'] ?? '', index);
+                      }
+                    },
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
   }
 }
-// class Sahifa extends StatefulWidget {
-//   @override
-//   State<Sahifa> createState() => _SahifaState();
-// }
-
-// class _SahifaState extends State<Sahifa> {
-//   List<Map<String, dynamic>> data = [];
-//   bool kutish = true;
-
-//   Future<void> fetchData() async {
-//     final response = await http.get(
-//       Uri.parse("https://api.alquran.cloud/v1/quran/uz.sodik"),
-//     );
-
-//     if (response.statusCode == 200) {
-//       final jsondata = jsonDecode(response.body);
-//       final surahs = jsondata['data']['surahs'] as List;
-//       setState(() {
-//         data = surahs.map((item) => Map<String, dynamic>.from(item)).toList();
-//       });
-//     } else {
-//       throw Exception("Failed to load data");
-//     }
-//   }
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     fetchData();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Column(
-//         children: [
-//           SizedBox(height: 50),
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               Text(
-//                 "Quran App",
-//                 style: TextStyle(
-//                   fontWeight: FontWeight.w900,
-//                   fontSize: 30,
-//                   color: Color.fromARGB(255, 0, 255, 8),
-//                 ),
-//               ),
-//             ],
-//           ),
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               Center(
-//                 child: Text(
-//                   "Assalomu Aleykum \n    va rahmatulloh",
-//                   style: TextStyle(
-//                     fontSize: 25,
-//                     color: Color.fromARGB(255, 0, 255, 8),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//           Container(
-//             height: 235,
-//             width: 350,
-//             decoration: BoxDecoration(
-//               boxShadow: [
-//                 BoxShadow(
-//                   color: Color.fromARGB(
-//                     255,
-//                     0,
-//                     255,
-//                     8,
-//                   ),
-//                   offset: Offset(
-//                     5,
-//                     5,
-//                   ),
-//                   blurRadius: 10,
-//                 ),
-//               ],
-//               borderRadius: BorderRadius.circular(25),
-//               color: Colors.grey[100],
-//             ),
-//             child: Padding(
-//               padding: EdgeInsets.all(8.5),
-//               child: Column(
-//                 children: [
-//                   Row(
-//                     children: [
-//                       Column(
-//                         children: [
-//                           Text(
-//                             "Quroni karimning\n  30 Pora",
-//                             style: TextStyle(
-//                               // fontSize: 0,
-//                               fontWeight: FontWeight.w900,
-//                             ),
-//                           ),
-//                         ],
-//                       ),
-//                       Container(
-//                         height: 180,
-//                         width: 140,
-//                         decoration: BoxDecoration(
-//                           image: DecorationImage(
-//                             image: NetworkImage(
-//                               'https://i.pinimg.com/564x/e2/8b/35/e28b3500f009332209532513119f80a2.jpg',
-//                             ),
-//                             fit: BoxFit.fitHeight,
-//                           ),
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//           SizedBox(
-//             height: 30,
-//           ),
-//           Expanded(
-//             child: ListView.builder(
-//               itemCount: data.length,
-//               itemBuilder: (context, index) {
-//                 return GestureDetector(
-//                   onTap: () {
-//                     Navigator.push(
-//                       context,
-//                       MaterialPageRoute(
-//                         builder: (context) =>
-//                             SurahDetail(surahData: data[index]),
-//                       ),
-//                     );
-//                   },
-//                   child: Padding(
-//                     padding: EdgeInsets.all(15),
-//                     child: Container(
-//                       height: 150,
-//                       decoration: BoxDecoration(
-//                         borderRadius: BorderRadius.circular(20),
-//                         color: Colors.white,
-//                       ),
-//                       child: Padding(
-//                         padding: EdgeInsets.all(10),
-//                         child: Column(
-//                           crossAxisAlignment: CrossAxisAlignment.start,
-//                           children: [
-//                             Text(
-//                               "${data[index]['englishName']}",
-//                               style: TextStyle(
-//                                 fontSize: 22,
-//                                 fontWeight: FontWeight.bold,
-//                               ),
-//                             ),
-//                             Text(
-//                               "${data[index]['name']}",
-//                               style: TextStyle(
-//                                 fontSize: 18,
-//                                 color: Colors.grey[700],
-//                               ),
-//                             ),
-//                             SizedBox(height: 5),
-//                             Text(
-//                               "${data[index]['revelationType']}",
-//                               style: TextStyle(
-//                                 fontSize: 16,
-//                                 color: Colors.grey[600],
-//                               ),
-//                             ),
-//                           ],
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                 );
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class SurahDetail extends StatelessWidget {
-//   final Map<String, dynamic> surahData;
-
-//   SurahDetail({required this.surahData});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final ayahs = surahData['ayahs'] as List<dynamic>;
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(surahData['englishName']),
-//       ),
-//       body: Column(
-//         children: [
-//           Slider(
-//                 min: 0,
-//                 max: duration.inSeconds.toDouble(),
-//                 // value: position.inSeconds.toDouble(),
-//                 value: position.inSeconds
-//                     .toDouble()
-//                     .clamp(0.0, duration.inSeconds.toDouble()),
-//                 onChanged: (value) async {
-//                   final newPosition = Duration(seconds: value.toInt());
-//                   await audioplayer.seek(newPosition);
-//                   setState(() {
-//                     position = newPosition;
-//                   });
-//                 },
-//               ),
-//           Expanded(
-//             child: ListView.builder(
-//               itemCount: ayahs.length,
-//               itemBuilder: (context, index) {
-//                 final ayah = ayahs[index];
-//                 return Padding(
-//                   padding: EdgeInsets.all(8.0),
-//                   child: ListTile(
-//                     title: Text(
-//                       "${ayah['text']}",
-//                       style: TextStyle(fontSize: 20),
-//                     ),
-//                   ),
-//                 );
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
